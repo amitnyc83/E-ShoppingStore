d3f8973465277c2672f4d81fb62262c2063c94cb
amitnyc83 <amit_nyc@live.com>
2019-06-10 15:18:51 -0400

    adds icons for home/cart & sign in

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react'
-import {Menu, Header, Icon } from 'semantic-ui-react'
+import { Input, Menu, Segment } from 'semantic-ui-react'
 
 export default class HomePage extends Component {
   state = {activeItem: 'home'}
+
+  handleMenuClick = (e, { name }) => this.setState({ activeItem: name })
 
   render (){
     const { activeItem } = this.state
 
     return (
-      <Menu>
+      <Menu attached='top' tabular>
        <Menu.Item
-       name='home'
-       color='green'
-       active={activeItem === 'home'}
-       onClick={this.handleMenuClick}
-      /></Menu>
+         name='home'
+         color='orange'
+         active={activeItem === 'home'}
+         onClick={this.handleMenuClick}
+        />
+        <Menu.Item
+          name='cart'
+          color='orange'
+          active={activeItem === 'cart'}
+          onClick={this.handleMenuClick}
+        />
+        <Menu.Item
+         name='sign-in'
+         color='orange'
+         active={activeItem === 'sign-in'}
+         onClick={this.handleMenuClick}
+        >
+        </Menu.Item>
+        <Menu.Menu position='right'>
+         <Menu.Item>
+           <Input
+             transparent
+             icon={{ name: 'search', link: true }}
+             placeholder='Search products...'
+            />
+         </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     )
   }
 }
