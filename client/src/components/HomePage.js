2454597bfa282c89c908426f94e8c88ef1fdd4c5
amitnyc83 <amit_nyc@live.com>
2019-06-11 18:38:14 -0400

    adds header and divider

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import { Input, Menu, Segment } from 'semantic-ui-react'
-import Logo from './Logo'
+import { Input, Sticky, Menu, Segment, Image, Divider } from 'semantic-ui-react'
+import _ from 'lodash'
+
 
 export default class HomePage extends Component {
   state = {activeItem: 'home'}
+
+
 
   handleMenuClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -11,36 +14,39 @@ export default class HomePage extends Component {
     const { activeItem } = this.state
 
     return (
-      <Menu attached='top' tabular>
-       <Menu.Item
-         name='home'
-         color='orange'
-         active={activeItem === 'home'}
-         onClick={this.handleMenuClick}
-        />
-        <Menu.Item
-          name='cart'
-          color='orange'
-          active={activeItem === 'cart'}
-          onClick={this.handleMenuClick}
-        />
-        <Menu.Item
-         name='sign-in'
-         color='orange'
-         active={activeItem === 'sign-in'}
-         onClick={this.handleMenuClick}
-        >
-        </Menu.Item>
-        <Menu.Menu position='right'>
-         <Menu.Item>
-           <Input
-             transparent
-             icon={{ name: 'search', link: true }}
-             placeholder='Search products...'
+      <Sticky>
+        <Menu attached='top' tabular>
+          <Menu.Item
+            name='home'
+            color='orange'
+            active={activeItem === 'home'}
+            onClick={this.handleMenuClick}
             />
-         </Menu.Item>
-        </Menu.Menu>
-      </Menu>
+          <Menu.Item
+            name='cart'
+            color='orange'
+            active={activeItem === 'cart'}
+            onClick={this.handleMenuClick}
+            />
+          <Menu.Item
+            name='sign-in'
+            color='orange'
+            active={activeItem === 'sign-in'}
+            onClick={this.handleMenuClick}
+            >
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Input
+                transparent
+                icon={{ name: 'search', link: true }}
+                placeholder='Search products...'
+                />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <Divider hidden />
+      </Sticky>
     )
   }
 }
--- client/src/containers/ProductsList.js
+++ client/src/containers/ProductsList.js
@@ -1,44 +1,74 @@
 import React from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Header, Divider } from 'semantic-ui-react'
 
 const ProductsList = () => (
+  <div>
+  <Divider hidden />
+  <Header as='h2' textAlign='center'> Products
+  <Divider hidden />
+  </Header>
   <Grid columns={3} divided>
-    <Grid.Row>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-    </Grid.Row>
+  <Grid.Row>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  </Grid.Row>
 
-    <Grid.Row>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-      </Grid.Column>
-    </Grid.Row>
+  <Grid.Row>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+  </Grid.Column>
+  </Grid.Row>
 
-    <Grid.Row>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-      </Grid.Column>
-    </Grid.Row>
+  <Grid.Row>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+  </Grid.Column>
+  </Grid.Row>
+
+  <Grid.Row>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+  </Grid.Column>
+  </Grid.Row>
+
+  <Grid.Row>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+  <Grid.Column>
+  <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+  </Grid.Column>
+  </Grid.Row>
   </Grid>
+  </div>
 )
 
 export default ProductsList
