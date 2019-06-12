ae36f5dd1da1a4c797b85a0d1a799d84b122826f
amitnyc83 <amit_nyc@live.com>
2019-06-11 20:53:02 -0400

    adds router to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './components/HomePage'
 import ProductsList from './containers/ProductsList'
@@ -9,13 +10,15 @@ import ProductsList from './containers/ProductsList'
 class App extends Component {
   render(){
     return (
+      <Router>
       <div className="App">
         <header>
         <h1 className="app-title">E-Shopping Store</h1>
         </header>
         <HomePage />
-        <ProductsList />
+        <Route exact path="/" component={ProductsList}/>
       </div>
+    </Router>
     )
   }
 }
