cafb4c3a1cf2132019555c10766b18957387164b
amitnyc83 <amit_nyc@live.com>
2019-06-20 22:30:25 -0400

    style ProductInfo Component

--- client/src/components/ProductInfo.js
+++ client/src/components/ProductInfo.js
@@ -7,6 +7,8 @@ class ProductInfo extends Component {
     return(
       <div className='product-div'>
        <img className='product-img' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+       <a href='#'><h4>Buy Now</h4></a>
+       <em>$49.99</em>
       </div>
     )
   }
